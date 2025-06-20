bcae88a06f1c5a4888be364b23a6478891a2ecd5
Roushan Goswami <roushangoswami1378@gmail.com>
2025-06-20 15:49:02 +0530

    Add files via upload

---
+++ C Language/arithmeticoperator.c
@@ -0,0 +1,18 @@
+
+// WAP to perform all arithmetic operator such as
+//+, -, *, /, %
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+    int a, b; // program for a+b
+    printf("\n");
+    printf("\n");
+    printf("Enter a :"); // user will enter the value of a here
+    scanf("%d", &a);
+    printf("Enter b :"); // user will enter the value of b here
+    scanf("%d", &b);
+    // final output
+    printf("a+b = %d", a + b);
+}
