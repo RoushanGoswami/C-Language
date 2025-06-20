e101e1dc14b1b339fb0bd5e024ae84e03212a18e
Roushan Goswami <roushangoswami1378@gmail.com>
2025-06-20 18:37:26 +0530

    Add files via upload

---
+++ C Language/operatos hw/opdivide.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main ()
+
+{
+	float a , b ;
+	printf("Enter a value a : ");//here the user will enter the value of a
+	scanf("%f",&a);
+	printf("Enter a value b : ");//here the user will enter the value of b
+	scanf("%f",&b);
+	printf("(a/b)= %f",a/b);//here the user gets the required output
+	
+	
+	
+	
+}
