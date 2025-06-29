9f644f858a4c4a362fd3a841d6cc947a9fdcb96e
Roushan Goswami <roushangoswami1378@gmail.com>
2025-06-29 14:09:10 +0530

    Update loop8wh.c

--- LOOP/loop8wh.c
+++ LOOP/loop8wh.c
@@ -6,16 +6,16 @@ void main ()
 {
 	//wap to print sum from 1 to N using while loop
 	
-	int i = 1, sum = 1; //declared a variable i=1 start point and sum = 1
+	int i = 1, sum = 0; //declared a variable i=1 start point and sum = 0
     int n;//declare a variable n
 	printf("Enter n : ");//user input message
 	scanf("%d",&n);//user input
 	
 	while (i<= n)//end point n ,condition
     {
-    	sum = sum * i ;
+    	sum = sum + i ;
     	i++;//increment
 	}	
-	printf("Factorial is %d\n",sum);//final output --->print
+	printf("sum is %d\n",sum);//final output --->print
 }
 
