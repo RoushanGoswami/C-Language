4c7a4802d23912ed82db84a143ea3ea567e88063
Roushan Goswami <roushangoswami1378@gmail.com>
2025-06-29 14:10:49 +0530

    Add files via upload

---
+++ LOOP/Do while Loop/dowhloop7.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    // wap to find leap year from 2000 to 3000
+   int start = 2000 , end = 3000;
+   do {
+   printf("%d\n",start);
+   start += 4;
+   
+   }while (start<=end);
+    
+    
+    
+}
