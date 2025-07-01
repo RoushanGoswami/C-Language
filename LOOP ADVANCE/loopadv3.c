df7d3d6a367b0c0be1ed2df02cc351f3d4444e2f
Roushan Goswami <roushangoswami1378@gmail.com>
2025-07-01 16:33:23 +0530

    Update loopadv3.c

--- LOOP ADVANCE/loopadv3.c
+++ LOOP ADVANCE/loopadv3.c
@@ -5,7 +5,7 @@ void main()
 {
     // wap to find sum of all digits of a number.
     int num;        // declared a variable num
-    int fd = 0, ld; // declare fd = 0 and ld variable
+    int sum = 0, ld; // declare sum = 0 and ld variable
 
     printf("Enter Number :");
     scanf("%d", &num);
@@ -13,8 +13,8 @@ void main()
     while (num > 0) // loop will not be executed when the condition become false
     {
         ld = num % 10;  // last digit will be identified here
-        num = num / 10; // last digit of num will be removed
-        fd = fd + ld;   // sum of fd and ld is assigned in fd
+        sum = sum + ld;   // sum += ld
+        num /= 10 ;
     }
-    printf("%d", fd); // final output
+    printf("%d", sum); // final output
 }
