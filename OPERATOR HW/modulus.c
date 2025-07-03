407ea61065782087dd798c46387484931b48ece1
Roushan Goswami <roushangoswami1378@gmail.com>
2025-07-03 16:48:23 +0530

    Create modulus.c

---
+++ OPERATOR HW/modulus.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main ()
+
+{
+	int a , b ;
+	printf("Enter a value a : ");//here the user will enter the value of a
+	scanf("%d",&a);
+	printf("Enter a value b : ");//here the user will enter the value of b
+	scanf("%d",&b);
+	printf("(a%b)=%d",a%b);//here the user gets the required output
+	
+	
+	
+	
+}
