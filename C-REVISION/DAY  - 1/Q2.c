e17ed8aad2034aa2985cd24c2ec155e64b968e00
Roushan Goswami <roushangoswami1378@gmail.com>
2025-12-31 18:09:09 +0530

    Add files via upload

---
+++ C-REVISION/DAY  - 1/Q2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main()
+{
+    // 2. WAP to Find max. from given 2 no. using if else
+
+    int a = 100, b = 30;
+    if (a > b)
+    {
+        printf(" a is max ! ");
+    }
+    else
+    {
+        printf("b is max ! ");
+    }
+}
