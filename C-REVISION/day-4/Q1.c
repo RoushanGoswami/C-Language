ea5ae7c82b925ec9b82d709b20b66d3149fe68cc
Roushan Goswami <roushangoswami1378@gmail.com>
2026-01-12 19:02:30 +0530

    Add files via upload

---
+++ C-REVISION/day-4/Q1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void main()
+{
+    // A B C D E
+    //   A B C D
+    //     A B C
+    //       A B
+    //         A
+
+    char i, j, k;
+
+    for (i = 'A'; i <= 'E'; i++)
+    {
+        for (k = 'E'; k > i; k--)
+        {
+            printf("  ");
+        }
+        for (j = 'A'; j <= i; j++)
+        {
+            printf("%c ", j);
+        }
+        printf("\n");
+    }
+ 
+}
